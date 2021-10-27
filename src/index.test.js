0ad13803df4d350450da7263b471cb0b2805217b
irrationnelle <drakkarverenis@gmail.com>
2021-10-28 02:26:48 +0900

    test: Modify expect to pass test in index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,12 @@
 import showGreeting from "index";
 import { getByText } from "@testing-library/dom";
+import { $el } from "fxdom";
 import { go } from "fxjs";
 import "@testing-library/jest-dom";
 
 test("hello world 를 표시한다.", () => {
+  const container = $el(`<div id="root"></div>`);
   go(container, showGreeting);
   const greeting = getByText(container, "hello world!");
-  expect(greeting).toBeInTheDocument();
+  expect(container).toContainElement(greeting);
 });
