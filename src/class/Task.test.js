43b54654a42cb575bec82df5231083c100cadc9d
irrationnelle <drakkarverenis@gmail.com>
2021-10-28 21:32:36 +0900

    test: Add test to check status of task

--- src/class/Task.test.js
+++ src/class/Task.test.js
@@ -5,3 +5,18 @@ test("Task has content and status", () => {
   expect(task.content).toMatch("Do homework");
   expect(task.status).toMatch("pending");
 });
+
+test("Task 는 pending, inProgress, completed 세가지 status 만을 가진다.", () => {
+  const pendingTask = new Task("Earn money", "pending");
+
+  expect(pendingTask.status).toMatch('pending');
+
+  const inProgressTask = new Task("Do study", "inProgress");
+  expect(inProgressTask.status).toMatch('inProgress');
+
+  const compltetedTask = new Task("Do test", "completed");
+  expect(compltetedTask.status).toMatch('completed');
+
+  const initializingOddTask = () => {new Task("Connect to HotS", "immune")};
+  expect(initializingOddTask).toThrowError()
+});
