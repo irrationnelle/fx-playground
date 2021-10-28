b8965774aebec3080d2d5aa1cbf80d3f07fb7f17
irrationnelle <drakkarverenis@gmail.com>
2021-10-28 13:22:53 +0900

    test: Add test by triangulation technic to implement actual method

--- src/class/TaskManager.test.js
+++ src/class/TaskManager.test.js
@@ -7,3 +7,12 @@ test("TaskManager 로 task 를 추가한다.", () => {
   taskManager.add(task);
   expect(taskManager.getTasks().length).toEqual(1);
 });
+
+test("TaskManager 는 추가한 task 와 동일한 task 를 가진 목록을 반환한다.", () => {
+  const taskManager = new TaskManager();
+  const task = new Task("Do test", "inProgress");
+  taskManager.add(task);
+  const [latestTask] = taskManager.getTasks();
+  expect(latestTask.content).toMatch("Do test");
+  expect(latestTask.status).toMatch("inProgress");
+});
