eeb79ff87e39551f25fd712103879fe0c4630622
irrationnelle <drakkarverenis@gmail.com>
2021-10-28 21:39:15 +0900

    feat: Throw error when constructor would be initialized with inappropriate argument for status

--- src/class/Task.js
+++ src/class/Task.js
@@ -1,6 +1,10 @@
 class Task {
   constructor(content, status) {
     this.content = content;
+
+    if (status !== 'pending' && status !== 'inProgress' && status !== 'completed') {
+      throw new Error('inappropriate status')
+    }
     this.status = status;
   }
 }
