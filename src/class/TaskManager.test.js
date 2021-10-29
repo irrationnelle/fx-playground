e83f3ee912427e397efbe14a1a16763c0398a480
irrationnelle <drakkarverenis@gmail.com>
2021-10-29 21:25:16 +0900

    test: Skip remove operation test to create other prior test

--- src/class/TaskManager.test.js
+++ src/class/TaskManager.test.js
@@ -36,7 +36,7 @@ test("TaskManager 는 특정 status 를 가지고 있는 task 만을 가진 목�
   }
 })
 
-test("TaskManager 는 tasks 목록에 있는 task 를 삭제하고 최신화된 tasks 목록을 받아올 수 있다.", () => {
+test.skip("TaskManager 는 tasks 목록에 있는 task 를 삭제하고 최신화된 tasks 목록을 받아올 수 있다.", () => {
   const taskManager = new TaskManager();
 
   const pendingTask = new Task("Earn money", "pending");
