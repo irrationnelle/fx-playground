76ccc66cb234bb6e4e37f4809316029aac8e6612
irrationnelle <drakkarverenis@gmail.com>
2021-10-31 21:28:55 +0900

    test: Add test to check whether removed task should be removed from DOM too

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,9 @@
-import { showGreeting, Tasks$ } from "index";
-import { getByText } from "@testing-library/dom";
-import { $appendTo, $el } from "fxdom";
+import { render, showGreeting, Tasks$ } from "index";
+import { getByText, queryByText } from "@testing-library/dom";
+import { $el } from "fxdom";
 import { go } from "fxjs";
 import "@testing-library/jest-dom";
+
 import TaskManager from "./class/TaskManager";
 import Task from "./class/Task";
 
@@ -37,4 +38,30 @@ test("Task 목록을 html로 표시한다.", () => {
 
   const statusByTask = getByText(container, "completed");
   expect(container).toContainElement(statusByTask);
+});
+
+test("Task 목록에서 지워진 task 는 html 에서도 삭제한다.", () => {
+  // given
+  const taskManager = new TaskManager();
+
+  const pendingTask = new Task("Earn money", "pending");
+  const studyTask = new Task("Do study", "inProgress");
+  const guitarTask = new Task("Play the guitar", "inProgress");
+  const compltetedTask = new Task("Do test", "completed");
+
+  taskManager.add(pendingTask);
+  taskManager.add(studyTask);
+  taskManager.add(guitarTask);
+  taskManager.add(compltetedTask);
+
+  const container = $el(`<div id="root"></div>`);
+  render("tasks", taskManager.getTasks(), Tasks$.tmpl, container);
+
+  // when
+  taskManager.removeTask(studyTask.id);
+  render("tasks", taskManager.getTasks(), Tasks$.tmpl, container);
+
+  // then
+  const removedTask = queryByText(container, "Do study");
+  expect(removedTask).not.toBeTruthy();
 });
