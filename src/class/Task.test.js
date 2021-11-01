9bdb7fe35026066e39a7c6c35f2014e31c8e184d
irrationnelle <drakkarverenis@gmail.com>
2021-11-02 02:22:55 +0900

    test: Add test to check operation that modifies content of existing task

--- src/class/Task.test.js
+++ src/class/Task.test.js
@@ -9,19 +9,34 @@ test("Task has content and status", () => {
 test("Task 는 pending, inProgress, completed 세가지 status 만을 가진다.", () => {
   const pendingTask = new Task("Earn money", "pending");
 
-  expect(pendingTask.status).toMatch('pending');
+  expect(pendingTask.status).toMatch("pending");
 
   const inProgressTask = new Task("Do study", "inProgress");
-  expect(inProgressTask.status).toMatch('inProgress');
+  expect(inProgressTask.status).toMatch("inProgress");
 
   const compltetedTask = new Task("Do test", "completed");
-  expect(compltetedTask.status).toMatch('completed');
+  expect(compltetedTask.status).toMatch("completed");
 
-  const initializingOddTask = () => {new Task("Connect to HotS", "immune")};
-  expect(initializingOddTask).toThrowError()
+  const initializingOddTask = () => {
+    new Task("Connect to HotS", "immune");
+  };
+  expect(initializingOddTask).toThrowError();
 });
 
 test("Task 는 자신의 id 를 가진다.", () => {
-  const task = new Task("Listen to the new album of Dream Theater", 'inProgress');
+  const task = new Task(
+    "Listen to the new album of Dream Theater",
+    "inProgress"
+  );
   expect(task.id).toBeTruthy();
-})
+});
+
+test("Task 의 content 를 변경할 수 있다..", () => {
+  const task = new Task(
+    "Listen to the new album of Dream Theater",
+    "inProgress"
+  );
+  const newContent = "Listen to the new album of Dark Tranquillity";
+  task.modifyContent(newContent);
+  expect(task.content).toMatch(newContent);
+});
